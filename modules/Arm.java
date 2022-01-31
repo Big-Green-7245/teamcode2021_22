c1974b6b52ed96a4c1a7ce86cf0d96027945d2d5
Alice <58663120+Alice-Xzq@users.noreply.github.com>
2022-01-31 16:30:19 -0500

    update Arm, Nav, AADC, and AliceTest

--- modules/Arm.java
+++ modules/Arm.java
@@ -1,38 +1,61 @@
-//Note one motor for extension vertically
+package org.firstinspires.ftc.teamcode.modules;
 
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-//This is the interface for the arm
-//using one motor for extension (up) and (down)
-// - Ros 10/5/21
+import org.firstinspires.ftc.teamcode.modules.*;
+import org.firstinspires.ftc.teamcode.util.*;
 
-public class Arm {
-
-  private DcMotor armMotor;
-
-  public Arm(DcMotor inputMotor){
-    armMotor = inputMotor;
-  }
-
-// make the arm rise
-  private void rise(){
-    armMotor.setDirection(DcMotor.Direction.FORWARD);
-    armMotor.setPower(1);
-  }
-
-  //make the arm lower
-  private void lower(){
-    armMotor.setDirection(DcMotor.Direction.REVERSE);
-    armMotor.setPower(1);
-  }
+public class Arm
+{
+    public DcMotor Lever;
+    public Servo BoxJoint;
+    public CRServo BoxFinger;
+    private boolean isDropped = true;
+    
+    private final double SERVO_FETCH_POS = 0.6;
+    private final double SERVO_DROP_POS = 0.4;
+    
+    public boolean isAtFetchPos = true;
+    
+    public Arm(HardwareMap hwMap)
+    {
+        BoxJoint = hwMap.get(Servo.class, "boxJoint");
+        BoxFinger = hwMap.get(CRServo.class, "boxFinger");
+        BoxFinger.setDirection(DcMotorSimple.Direction.FORWARD);
+        
+        setBoxFetch(isAtFetchPos);
+        
+        Lever = hwMap.get(DcMotor.class, "arm");
+        Lever.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    
+    public void MoveArm(double power)
+    {
+        Lever.setPower(power);
+    }
+    
+    public void TiltBox(double power)
+    {
+        //BoxJoint.setPower(power);
+    }
+    
+    public void setBoxFetch(boolean state) {
+        if(state) BoxJoint.setPosition(SERVO_FETCH_POS);
+        else BoxJoint.setPosition(SERVO_DROP_POS);
+    }
+    
+    public void toggleBoxStatus() {
+        isAtFetchPos = !isAtFetchPos;
+        setBoxFetch(isAtFetchPos);
+    }
+    
+    public void SpinFinger(double power)
+    {
+        BoxFinger.setPower(power);
+    }
 }
